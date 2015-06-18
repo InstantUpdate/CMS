17f0994bf4df3ef774f6103c44a50f00085adb75
Nemanja Avramovic <avramyu@gmail.com>
2015-06-18 20:00:06 +0200

    removed newline in frontend toolbars

--- iu-resources/js/functions.js
+++ iu-resources/js/functions.js
@@ -74,8 +74,7 @@ function enable_snapeditor($el)
 	CKEDITOR.inline(elid, {
 	toolbar: [
 		{ name: 'document', items: [ 'Source', '-', 'NewPage', 'Preview', '-', 'Templates' ] },	// Defines toolbar group with name (used to create voice label) and items in 3 subgroups.
-		[ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ],			// Defines toolbar group without name.
-		'/',																					// Line break - next group will be placed in new line.
+		[ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ],			// Defines toolbar group without name.,																					// Line break - next group will be placed in new line.
 		{ name: 'basicstyles', items: [ 'Bold', 'Italic' ] }
 	]});
 	$el.addClass('iu-editable');
