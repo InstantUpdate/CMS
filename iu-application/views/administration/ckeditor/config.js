cfa958ceef2d0f983521e93dac3ee607a6825cb6
Tomo Vukasović <webinsane@gmail.com>
2015-06-30 11:58:37 +0200

    CK Editor Content fix and dark style updated

--- iu-application/views/administration/ckeditor/config.js
+++ iu-application/views/administration/ckeditor/config.js
@@ -7,5 +7,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	config.skin = 'flat';
+	config.skin = 'moono-dark';
+	config.allowedContent = true;
 };
