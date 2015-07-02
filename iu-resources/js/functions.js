d1a19a20f24e5301d4435936f4b2c878e0fa8235
Tomo Vukasović <webinsane@gmail.com>
2015-07-02 14:40:09 +0200

    Icons update

--- iu-resources/js/functions.js
+++ iu-resources/js/functions.js
@@ -75,7 +75,7 @@ function enable_snapeditor($el)
 		toolbar: [
 			{ name: 'document', items: [ 'Inlinesave', '-', 'NewPage', '-', 'Templates', 'Advanced' ] },	// Defines toolbar group with name (used to create voice label) and items in 3 subgroups.
 			[ 'Cut', 'Copy', 'Paste', 'PasteText', '-', 'Undo', 'Redo' ],			// Defines toolbar group without name.,																					// Line break - next group will be placed in new line.
-			{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Link', 'Image', 'addImage' ] }
+			{ name: 'basicstyles', items: [ 'Bold', 'Italic','TextColor', 'Link', 'Image', 'addImage', 'Youtube' ] }
 		]
 	});
 	$el.addClass('iu-editable');
